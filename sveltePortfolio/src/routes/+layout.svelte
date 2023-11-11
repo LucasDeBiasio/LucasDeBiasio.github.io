17de7e77c48f50d1302ae0bbfdd259c0fb067cbd
Lucas <lucasnoeldeb@gmail.com>
2023-11-10 19:31:29 -0800

    adding other files

---
+++ sveltePortfolio/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import "../app.css";
+    import Main from "./components/Main.svelte";
+    import Header from "./components/Header.svelte";
+    import Footer from "./components/Footer.svelte";
+    
+</script>
+
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+    <Header/>
+    <slot />
+    <Footer/>
+</div>
+
+
+  
