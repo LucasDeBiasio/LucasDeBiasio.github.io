e6b030646a1c7153d99fa418d4943a77ea844e3d
Lucas <lucasnoeldeb@gmail.com>
2023-11-21 22:12:24 -0800

    Issue #5, created resume page

--- sveltePortfolio/src/routes/resume/+page.svelte
+++ sveltePortfolio/src/routes/resume/+page.svelte
@@ -2,4 +2,15 @@
     <div class="mx-40 font-black mt-16 Roboto text-7xl text-center width-100">
         <h1>Resumé</h1>
     </div>
+    <div class="flex justify-center">
+        <object
+        class="mb-20 mt-5"
+        type="application/pdf"
+        title="My Resumé"
+        data="resume.pdf"
+        width="600"
+        height="700"
+        >
+        </object>
+    </div>
 </div>
