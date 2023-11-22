f8d75b6bac09fcf29825ffa85251af695bcf8c26
Lucas <lucasnoeldeb@gmail.com>
2023-11-21 16:34:57 -0800

    Issue #2, integrated carousel into main page

--- sveltePortfolio/src/routes/+page.svelte
+++ sveltePortfolio/src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script>
     import Carousel from "./components/Carousel.svelte";
 
-    let name = 'world';
-    console.log(name);
-
     let socials = [
         {name: 'linkedIn', link: 'https://www.linkedin.com/in/lucasdebiasio/', icon: 'fa-brands fa-linkedin'},
         {name: 'github', link: 'https://github.com/LucasDeBiasio', icon: 'fa-brands fa-github'},
@@ -16,10 +13,11 @@
     <div class="mx-40 gap-10 flex">
         <div class="w-4/6">
             <h1 class="font-black text-7xl Roboto">Lucas <span class="uppercase"> De Biasio</span>,</h1>
-            <p class="text-xl font-light py-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer consectetur, turpis at placerat cursus, enim neque blandit dui, sit amet aliquet ex lectus sit amet lorem. Quisque sagittis risus quis dictum interdum. Fusce eget erat hendrerit ex egestas auctor et vitae est. Praesent sed ullamcorper erat, sed tempor justo.</p>
+            <p class="text-xl font-light py-2">My name is Lucas, I am a third year Computer Engineering student at Simon Fraser University who is also pursuing a Minor Degree in Computing Science. I enjoy creating with technology and learning new things. This page will act as an interactive resumé to showcase my projects and future technical work experience. Enjoy!
+            </p>
             <div class="font-light Roboto sm:flex items-center">
                 {#each socials as social, index}
-                    <div class="duration-200 px-2 rounded-lg hover:bg-slate-700">
+                    <div class="duration-200 px-2 rounded-lg hover:bg-neutral-700">
                         <a id="mod" class="flex flex-row items-center" target = "blank" href={social.link}>
                             <i class="{social.icon} px-1"></i>
                             <p>{social.name}</p>
