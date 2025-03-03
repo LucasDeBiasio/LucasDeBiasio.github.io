b729458899ef3409268827af7f76077e1969f074
Lucas De Biasio <101230663+LucasDeBiasio@users.noreply.github.com>
2025-03-03 12:45:19 -0800

    Update svelte.config.js

--- sveltePortfolio/svelte.config.js
+++ sveltePortfolio/svelte.config.js
@@ -3,7 +3,16 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    paths: {
+      base: '/sveltePortfolio'
+    },
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    })
   },
   preprocess: vitePreprocess()
 };
